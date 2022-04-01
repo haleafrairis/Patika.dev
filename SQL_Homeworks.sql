420b5e9bbbc64e11d08be4cd510fdc53b53b6a5f
Hale Afra İRİŞ <84947858+haleafraoztekin@users.noreply.github.com>
2022-04-01 18:32:12 +0300

    Update SQL_Homeworks.sql

--- SQL_Homeworks.sql
+++ SQL_Homeworks.sql
@@ -300,3 +300,35 @@ EXCEPT ALL
 (SELECT first_name FROM customer)
 ORDER BY first_name;
 ORDER BY first_name;
+
+
+-- HW12
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+-- Q1. film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+-- Q2. film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+-- Q3. film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+-- Q4. payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+-- Q1.
+SELECT count(*) FROM film
+WHERE length > 
+(SELECT AVG(length) FROM film);
+
+-- Q2.
+SELECT count(*) FROM film
+WHERE rental_rate = 
+(SELECT MAX(rental_rate) FROM film);
+
+-- Q3.
+SELECT * FROM film
+WHERE rental_rate = 
+(SELECT MIN(rental_rate) FROM film)
+AND replacement_cost =
+(SELECT MIN(replacement_cost) FROM film);
+
+-- Q4.
+SELECT payment.customer_id, first_name, last_name, COUNT(payment.customer_id) AS payment_count
+FROM payment
+FULL JOIN customer ON payment.customer_id = customer.customer_id
+GROUP BY payment.customer_id, first_name, last_name
+ORDER BY payment_count DESC;
