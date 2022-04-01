cc6fefd8640b5f6729f0aa287f84f41ba1756b0c
Hale Afra İRİŞ <84947858+haleafraoztekin@users.noreply.github.com>
2022-04-01 17:34:11 +0300

    Update SQL_Homeworks.sql

--- SQL_Homeworks.sql
+++ SQL_Homeworks.sql
@@ -256,3 +256,47 @@ RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
 -- Q3.
 SELECT first_name, last_name, rental_id FROM customer
 FULL JOIN rental ON customer.customer_id = rental.customer_id;
+
+
+-- HW11
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+-- Q1. actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+-- Q2. actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+-- Q3. actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+-- Q4. İlk 3 sorguyu tekrar eden veriler için de yapalım.
+
+
+-- Q1.
+(SELECT first_name FROM actor)
+UNION
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+
+-- Q2.
+(SELECT first_name FROM actor)
+INTERSECT
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+
+-- Q3.
+(SELECT first_name FROM actor)
+EXCEPT
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+
+--Q4.
+(SELECT first_name FROM actor)
+UNION ALL
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+--------------------------------
+(SELECT first_name FROM actor)
+INTERSECT ALL
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+--------------------------------
+(SELECT first_name FROM actor)
+EXCEPT ALL
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+ORDER BY first_name;
